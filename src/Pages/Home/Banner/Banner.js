80842937699e1b0bf64e3e12aa1628e8f73d3739
sadmanador <sadman.ador@gmail.com>
2023-03-20 23:23:13 +0600

    66-4 done!

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,18 +3,33 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
-          className="w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
+      <div id="slide1" className="carousel-item relative w-full text-white">
+        <img src="assets/images/banner/1.jpg" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-15 right-5 bottom-5">
+          <a href="#slide4" className="btn btn-circle bg-orange-500">
             ❮
           </a>
-          <a href="#slide2" className="btn btn-circle">
+          <a href="#slide2" className="btn btn-circle ml-10  bg-orange-500">
             ❯
           </a>
+        </div>
+        
+        <div className="absolute flex justify-between mt-20 ml-20">
+          <h1 className="text-6xl font-bold text-white">
+            Affordable <br />
+            Price for Car <br />
+            Servicing
+          </h1>
+        </div>
+        <div className="flex justify-between absolute top-80 ml-20">
+          <p className="">
+            There are many variations of passages of available, but the majority
+            have suffered alteration in some form
+          </p>
+        </div>
+        <div className="absolute flex justify-start left-24 top-3/4">
+          <button className="btn btn-error mr-5">Discover More</button>
+          <button className="btn btn-outline btn-error">Latest Product</button>
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
