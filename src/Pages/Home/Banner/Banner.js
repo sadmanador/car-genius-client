bd314bc23edda1cbd4d7f892c4b1e6677ed97f73
sadmanador <sadman.ador@gmail.com>
2023-03-20 23:37:52 +0600

    66-5 done!

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,79 +1,55 @@
 import React from "react";
+import "./Banner.css";
+import BannerItem from "./BannerItem/BannerItem";
+
+const bannerData = [
+    {
+        id: 1,
+        image: "assets/images/banner/1.jpg",
+        prev: 6,
+        next: 2,
+    },
+    {
+        id: 2,
+        image: "assets/images/banner/2.jpg",
+        prev: 1,
+        next: 3,
+    },
+    {
+        id: 3,
+        image: "assets/images/banner/3.jpg",
+        prev: 2,
+        next: 4,
+    },
+    {
+        id: 4,
+        image: "assets/images/banner/4.jpg",
+        prev: 3,
+        next: 5,
+    },
+    {
+        id: 5,
+        image: "assets/images/banner/5.jpg",
+        prev: 4,
+        next: 6,
+    },
+    {
+        id: 6,
+        image: "assets/images/banner/6.jpg",
+        prev: 5,
+        next: 1,
+    }
+]
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full text-white">
-        <img src="assets/images/banner/1.jpg" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-15 right-5 bottom-5">
-          <a href="#slide4" className="btn btn-circle bg-orange-500">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle ml-10  bg-orange-500">
-            ❯
-          </a>
-        </div>
-        
-        <div className="absolute flex justify-between mt-20 ml-20">
-          <h1 className="text-6xl font-bold text-white">
-            Affordable <br />
-            Price for Car <br />
-            Servicing
-          </h1>
-        </div>
-        <div className="flex justify-between absolute top-80 ml-20">
-          <p className="">
-            There are many variations of passages of available, but the majority
-            have suffered alteration in some form
-          </p>
-        </div>
-        <div className="absolute flex justify-start left-24 top-3/4">
-          <button className="btn btn-error mr-5">Discover More</button>
-          <button className="btn btn-outline btn-error">Latest Product</button>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-          className="w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src="/images/stock/photo-1414694762283-acccc27bca85.jpg"
-          className="w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img
-          src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
-          className="w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
+    <div className="carousel w-full py-10">
+      {
+        bannerData.map(slide=> <BannerItem
+        key={slide.id}
+        slide={slide}
+        ></BannerItem>)
+      }
     </div>
   );
 };
---
+++ src/Pages/Home/Banner/BannerItem/BannerItem.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const BannerItem = ({slide}) => {
+    const {id, image, prev, next} = slide;
+  return (
+    <div id={`slide${id}`} className="carousel-item relative w-full text-white">
+      <div className="carousel-img">
+        <img src={image} className="w-full" />
+      </div>
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-15 right-5 bottom-5">
+        <a href={`#slide${prev}`} className="btn btn-circle bg-orange-500">
+          ❮
+        </a>
+        <a href={`#slide${next}`} className="btn btn-circle ml-10  bg-orange-500">
+          ❯
+        </a>
+      </div>
+
+      <div className="absolute flex justify-between mt-20 ml-20">
+        <h1 className="text-6xl font-bold text-white">
+          Affordable <br />
+          Price for Car <br />
+          Servicing
+        </h1>
+      </div>
+      <div className="flex justify-between absolute top-80 ml-20">
+        <p className="">
+          There are many variations of passages of available, but the majority
+          have suffered alteration in some form
+        </p>
+      </div>
+      <div className="absolute flex justify-start left-24 top-3/4">
+        <button className="btn btn-error mr-5">Discover More</button>
+        <button className="btn btn-outline btn-error">Latest Product</button>
+      </div>
+    </div>
+  );
+};
+
+export default BannerItem;
