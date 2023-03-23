c99c517c038f6c5146193793b71f0e0d6e92f446
sadmanador <sadman.ador@gmail.com>
2023-03-23 18:34:35 +0600

    67-1 done!

--- src/Pages/Home/Service/ServiceCard.js
+++ src/Pages/Home/Service/ServiceCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsArrowRightShort } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const serviceCard = ({ service }) => {
   const { _id, img, title, price } = service;
@@ -17,7 +18,7 @@ const serviceCard = ({ service }) => {
         <h2 className="card-title font-bold text-2xl">{title}</h2>
         <p className="text-start text-xl text-orange-600 font-bold">Price: ${price}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-error btn-outline"><BsArrowRightShort/></button>
+          <Link to='' className="btn btn-error btn-outline"><BsArrowRightShort/></Link>
         </div>
       </div>
     </div>
